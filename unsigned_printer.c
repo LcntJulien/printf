13a79ffae7d8939b9bf87c7b4cb1535a9ed0a4df
Julio <73828002+LcntJulien@users.noreply.github.com>
2022-11-26 16:30:08 +0100

    Update unsigned_printer.c

--- unsigned_printer.c
+++ unsigned_printer.c
@@ -1,12 +1,57 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   output_types.c                                     :+:      :+:    :+:   */
+/*   unsigned_printer.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jlecorne <jlecorne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 15:40:26 by jlecorne          #+#    #+#             */
-/*   Updated: 2022/11/23 16:13:31 by jlecorne         ###   ########.fr       */
+/*   Updated: 2022/11/26 16:29:28 by jlecorne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
+int	nbrlen(long n)
+{
+	size_t	r;
+
+	r = 0;
+	while (n >= 1)
+	{
+		r++;
+		n /= 10;
+	}
+	return (r);
+}
+
+char	*unsigned_itoa(unsigned int c)
+{
+	int		i;
+	char	*r;
+
+	i = nbrlen(c);
+	if (c == 0)
+		return (r = ft_strdup("0"));
+	r = malloc(sizeof(char) * (i + 1));
+	if (!r)
+		return (NULL);
+	r[i] = '\0';
+	while (--i >= 0)
+	{
+		r[i] = (c % 10) + '0';
+		c /= 10;
+	}
+	return (r);
+}
+
+int	unsignedprinter(unsigned int c)
+{
+	int r;
+	char print;
+
+	r = 0;
+	print = unsigned_itoa(c);
+	r += strprinter(print);
+	free(print);
+}
